f2a8cef2654acc424472290735c84dd477e85b82
horstwurm <horst.wurm@bluewin.ch>
2016-11-28 18:34:53 +0000

    first

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 path = Rails.root.to_s + "/app/assets/images/"
-
+path = Rails.root.to_s + "/public/assets/"
 #create Params
 appparams = Appparam.create({name:"Kleinanzeigen", description:"Kleinanzeigen für Private Anbieten & Suchen", active:true})
 appparams = Appparam.create({name:"Stellenanzeigen", description:"Stellenanzeigen für Institutionen", active:true})
