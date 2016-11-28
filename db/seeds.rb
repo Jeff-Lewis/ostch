85236dce11e6c4c64ee27ecd747c288d8bd6b189
horstwurm <horst.wurm@bluewin.ch>
2016-11-28 21:15:52 +0000

    first

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 path = Rails.root.to_s + "/app/assets/images/"
-path = Rails.root.to_s + "/assets/"
+path = Rails.root.to_s + "/public/assets/"
 
 #create Params
 appparams = Appparam.create({name:"Kleinanzeigen", description:"Kleinanzeigen für Private Anbieten & Suchen", active:true})
