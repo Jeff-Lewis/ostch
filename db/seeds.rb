651584442bb8ca3654ef84ee22ab63eef755cf79
horstwurm <horst.wurm@bluewin.ch>
2016-11-29 08:46:50 +0000

    first

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-path = Rails.root.to_s + "/app/assets/images/"
+path = Rails.root.to_s + "/app/assets/images/samples/"
 
 #create Params
 appparams = Appparam.create({name:"Kleinanzeigen", description:"Kleinanzeigen für Private Anbieten & Suchen", active:true})
@@ -35,7 +35,7 @@ appparams = Appparam.create({name:"Accounts", description:"Konten für Kunden vo
 appparams = Appparam.create({name:"Transaktionen", description:"Transaktionen", active:true})
 
 #create some users...
-users = User.create({calendar:true, time_from:8, time_to:20, dateofbirth:"09.05.1963", anonymous:false, status:"ok", active:true, email:"Paul.Panzer2@bluewin.ch", password:"password", name:"Paul", lastname:"Panzer", address1:"Hörnliblick 11", address2:"Zezikon", address3:"Thurgau", superuser:true, webmaster:true, avatar:File.open(path+'ma_2.jpg', 'rb')})
+users = User.create({calendar:true, time_from:8, time_to:20, dateofbirth:"09.05.1963", anonymous:false, status:"ok", active:true, email:"Paul.Panzer5@bluewin.ch", password:"password", name:"Paul", lastname:"Schlumpf", address1:"Hörnliblick 11", address2:"Zezikon", address3:"Thurgau", superuser:true, webmaster:true, avatar:File.open(path+'ma_2.jpg', 'rb')})
 
 if false
 
